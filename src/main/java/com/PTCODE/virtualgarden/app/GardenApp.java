ade7e3859dad874675ce31ed23cfb79c8cb1afac
diogodcoimbra <diogo.coimbra2020@gmail.com>
2025-11-29 16:50:07 +0100

    GardenApp structure commit

---
+++ src/main/java/com/PTCODE/virtualgarden/app/GardenApp.java
@@ -0,0 +1,52 @@
+package com.PTCODE.virtualgarden.app;
+
+public class GardenApp {
+
+    //Attributes
+    private AuthenticationManger authManager;
+    private boolean isRunning;
+
+    //Constructor
+    public GardenApp() {
+        //Initialization of the attributes
+        this.authManager = new AuthenticationManger;
+        //the app starts not running
+        this.isRunning = false;
+
+        //Method to control the app
+        public void startApp() {
+
+        }
+
+        //Method to create gardens
+        public void createGarden(String name, Region region) {
+
+        }
+
+        //Method to delete gardens
+        public void deleteGarden(Garden garden) {
+
+        }
+
+        //Method to open gardens
+        public void openGarden(Garden garden) {
+
+        }
+
+        //Method to show login screen
+        public void showLoginScreen() {
+
+        }
+
+        //Method to show main menu
+        public void showMainMenu() {
+
+        }
+
+        //Method to logout
+        public void logout() {
+
+        }
+
+    }
+}

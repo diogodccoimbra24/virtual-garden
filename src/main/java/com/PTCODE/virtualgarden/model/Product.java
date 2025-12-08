482c1e89aadd192aac2df8e07833f68e2cb190db
diogodccoimbra24 <diogodccoimbra@gmail.com>
2025-12-08 14:53:37 +0100

    Product class completed

--- src/main/java/com/PTCODE/virtualgarden/model/Product.java
+++ src/main/java/com/PTCODE/virtualgarden/model/Product.java
@@ -18,6 +18,13 @@ public class Product {
     //Method to apply a product to the plant
     public void applyToPlant(Plant plant) {
 
+        if(plant == null) {
+            System.out.println("There is no plant to apply the product.");
+        }
+        else {
+            plant.applyProduct(this);
+            System.out.println("The " + name + " (product) was applied to " + plant.getName());
+        }
     }
 
     //Method to get the name of the plant (getter)

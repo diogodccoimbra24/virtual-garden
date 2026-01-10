8907b9445c171579ed5f5579c2027e8b07937c3e
diogodccoimbra24 <diogodccoimbra@gmail.com>
2026-01-10 18:53:31 +0100

    Garden app class debugging and completion + adding more getters

--- src/main/java/com/PTCODE/virtualgarden/app/GardenApp.java
+++ src/main/java/com/PTCODE/virtualgarden/app/GardenApp.java
@@ -1,57 +1,108 @@
 package com.PTCODE.virtualgarden.app;
 
 import com.PTCODE.virtualgarden.auth.AuthenticationManager;
+import com.PTCODE.virtualgarden.auth.User;
 import com.PTCODE.virtualgarden.model.Garden;
 import com.PTCODE.virtualgarden.model.Region;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
 public class GardenApp {
 
-    //Attributes
+    //Attributes DC
     private AuthenticationManager authManager;
-    private boolean isRunning;
+    private Garden currentGarden;
 
-    //Constructor
+    //Constructor DC
     public GardenApp() {
         //Initialization of the attributes
         this.authManager = new AuthenticationManager();
-        //the app starts not running
-        this.isRunning = false;
+        this.currentGarden = null;
     }
 
-    //Method to control the app
-    public void startApp() {
+    //Method to create gardens DC
+    public boolean createGarden(String name, Region region) {
+        User user = authManager.getCurrentUser();
 
+        //To check if the user is logged in DC
+        if (user == null){
+            return false;
+        }
+
+        //Uses the method from Garden DC
+        Garden garden = new Garden(name, region);
+        user.addGarden(garden);
+        return true;
     }
 
-    //Method to create gardens
-    public void createGarden(String name, Region region) {
+    //Method to delete gardens DC
+    public boolean deleteGarden(Garden garden) {
+        User user = authManager.getCurrentUser();
 
+        if (user == null || garden == null){
+            return false;
+        }
+        user.removeGarden(garden);
+        return true;
     }
 
-    //Method to delete gardens
-    public void deleteGarden(Garden garden) {
-
+    //Method to get the list of the current user garden's (getter)
+    public List<Garden> getCurrentUserGardens() {
+        User user = authManager.getCurrentUser();
+        if (user == null){
+            //If there is no user logged in it will return an empty list DC
+            return Collections.emptyList();
+        }
+        return user.getGardens();
     }
 
-    //Method to open gardens
-    public void openGarden(Garden garden) {
+    //Method to open gardens DC
+    public boolean openGarden(Garden garden) {
+        User user = authManager.getCurrentUser();
 
+        //If user is not logged in and there id no garden it's not possible to use the method to open the gardens DC
+        if (user == null || garden == null){
+            return false;
+        }
+        //If user doesn't have any garden, there is no gardens to open, he must create first DC
+        if (!user.getGardens().contains(garden)){
+            return false;
+        }
+
+        this.currentGarden = garden;
+        return true;
     }
 
-    //Method to show login screen
-    public void showLoginScreen() {
-
+    //Method to close the current garden in DC
+    public void closeGarden(){
+        this.currentGarden = null;
     }
 
-    //Method to show main menu
-    public void showMainMenu() {
-
+    //Method to get the current user (getter) DC
+    public Garden getCurrentGarden(){
+        return currentGarden;
     }
 
-    //Method to logout
+    //Method to show login DC
+    public boolean login(String username, String password) {
+        return authManager.login(username, password);
+    }
+
+    //Method to show register DC
+    public void register(String username, String password){
+        authManager.registerUser(username, password);
+    }
+
+    //Method to logout DC
     public void logout() {
-
+        authManager.logout();
+        currentGarden = null;
     }
 
-
+    public User getCurrentUser(){
+        return authManager.getCurrentUser();
+    }
 }
